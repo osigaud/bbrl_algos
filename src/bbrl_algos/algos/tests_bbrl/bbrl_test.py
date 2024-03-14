203e168f2126923cd7d2affa5066e66921062ee2
osigaud <Olivier.Sigaud@upmc.fr>
2024-03-14 10:26:20 +0100

    improved tests

--- src/bbrl_algos/algos/tests_bbrl/bbrl_test.py
+++ src/bbrl_algos/algos/tests_bbrl/bbrl_test.py
@@ -1,23 +1,12 @@
-import copy
-import torch
-import numpy as np
-
 from functools import partial
-
-from bbrl.utils.chrono import Chrono
 
 from bbrl.workspace import Workspace
 from bbrl.agents import Agents, TemporalAgent, PrintAgent
 
-from bbrl_algos.models.loggers import Logger
 from bbrl.utils.replay_buffer import ReplayBuffer
 
 from bbrl_algos.models.stochastic_actors import RandomContinuousOneDActor
-from bbrl_algos.models.actors import ContinuousDeterministicActor
 from bbrl.agents.gymnasium import make_env, ParallelGymAgent
-from gymnasium.wrappers import AutoResetWrapper
-
-import gymnasium as gym
 
 
 class BBRLTest:
@@ -28,18 +17,21 @@ class BBRLTest:
             include_last_state=True,
             seed=1,
         )
-        obs_size, act_size = self.env_agent.get_obs_and_actions_sizes()
-        # self.actor = ContinuousDeterministicActor(obs_size, [10, 10], act_size, seed=1)
         self.actor = RandomContinuousOneDActor()
         self.agent = TemporalAgent(Agents(self.env_agent, self.actor, PrintAgent()))
 
     def test(self):
         wp = Workspace()
-        taille = 1000
+        taille = 210
         self.agent(wp, n_steps=taille)
         assert wp.time_size() == taille
         tr_wp = wp.get_transitions()
-        assert tr_wp.time_size() == taille, f"real size = {wp.time_size()}"
+
+        print(tr_wp.variables["env/timestep"].size)
+        # assert tr_wp.time_size() == taille, f"real size = {tr_wp.time_size()}"
+        rb = ReplayBuffer(300)
+        rb.put(tr_wp)
+        print(rb.size(), rb.is_full)
 
 
 if __name__ == "__main__":
